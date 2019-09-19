ebbdc1f1f36a614198693a0dedb746f0dc36ef6a
Mike Thurgood <mikecthurgood@gmail.com>
2019-09-19 19:33:35 +0100

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,20 @@
 class ArtistsController < ApplicationController
+
+    get '/artists/?' do
+        @artists = Artist.all.sort_by(&:name)
+        erb :'artists/index'
+    end
+
+    get '/artists/:id/?' do
+        id = params[:id]
+        unless id.to_i == 0
+            @artist = Artist.find(id)
+        else
+            @artist = Artist.find_by_slug(id)
+        end
+        @songs = @artist.songs
+        @genres = @artist.genres
+        erb :'artists/show'
+    end
+
 end
